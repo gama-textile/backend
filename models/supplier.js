456fac3e2321e4d6bdc77765e053c135597a2da3
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-14 22:22:12 +0700

    Database

---
+++ models/supplier.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Supplier = sequelize.define('Supplier', {
+    name: DataTypes.STRING
+  }, {});
+  Supplier.associate = function(models) {
+    // associations can be defined here
+  };
+  return Supplier;
+};
