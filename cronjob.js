8a6c15cc1d40899eb282aaffc2339620121b050b
karyaTio <karya.tiosaputra@gmail.com>
2019-05-01 22:06:09 +0700

    Menghapus file tidak perlu

--- cronjob.js
+++
@@ -1,7 +0,0 @@
-var schedule = require('node-schedule');
-
-let startTime = new Date(Date.now());
-let endTime = new Date(startTime.getTime() + 60000);
-var j = schedule.scheduleJob({ start: startTime, end: endTime, rule: '*/10 * * * * *' }, function () {
-  console.log(`Time for tea! ${Date.now().toLocaleString()}`);
-});
